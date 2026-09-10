64ba06db7ed3362b8fba1bce1836a6d4f617e7b3
TTKIEU <86267201+TTKIEU@users.noreply.github.com>
2026-09-10 00:03:53 -0700

    Null Safety Checks/Exceptions for Delete by Id and Find by Id

--- backend/src/main/java/com/pulsegrid/backend/service/MonitoredServiceService.java
+++ backend/src/main/java/com/pulsegrid/backend/service/MonitoredServiceService.java
@@ -2,11 +2,12 @@ package com.pulsegrid.backend.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.pulsegrid.backend.model.MonitoredService;
 import com.pulsegrid.backend.repository.MonitoredServiceRepository;
-
 @Service
 public class MonitoredServiceService{
     //instead of creating another MonitoredServiceRepo object in this instance,
@@ -23,9 +24,14 @@ public class MonitoredServiceService{
         return repository.save(service);
     }
     public MonitoredService getServiceById(Long id){
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("Service Not Found"));
+        return repository.findById(id).orElseThrow(() -> 
+        //OLD method = flase server failure; returns 404 now
+            new ResponseStatusException(HttpStatus.NOT_FOUND, "Service Not Found"));
     }
     public void deleteService(Long id){
-        repository.deleteById(id);
+
+        MonitoredService monitoredService = repository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Service not found"));
+        repository.delete(monitoredService);
     }
 }
